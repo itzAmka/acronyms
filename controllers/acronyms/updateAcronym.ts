ff3a0208263a04434e6113ddd08690ad18a4f70e
Amka <Aminhassen1470@gmail.com>
2023-10-14 19:21:02 -0400

    impletement update acronym functionality

--- controllers/acronyms/updateAcronym.ts
+++ controllers/acronyms/updateAcronym.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import { prisma } from '../../config/prisma'
 
 /**
  * @controller Update Acronym
@@ -10,5 +11,55 @@ import asyncHandler from 'express-async-handler';
  * @access Public
  */
 export const updateAcronym = asyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Update acronym' });
+  // get id from req params
+  const { id } = req.params
+
+  // get data from req body
+  const { acronym, definition } = req.body
+
+  // check if acronym exists
+  const foundAcronym = await prisma.acronym.findUnique({
+    where: {
+      id,
+    }
+  }).catch((_) => { 
+    // if acronym does not exist, throw error
+    throw new Error(`Acronym with id \`${id}\` does not exist`)
+  })
+
+  // if the current acronym is same as the new acronym and the new acronym is not empty, skip the check
+  if (foundAcronym?.acronym !== acronym && acronym) { 
+    // check if the new acronym already exists in the database
+    const acronymExists = await prisma.acronym.findUnique({
+      where: {
+        acronym,
+      }
+    }).catch((_) => { 
+      // handle errors
+      throw new Error(`Error while checking if acronym \`${acronym}\` exists`)
+    })
+
+    // if acronym exists, throw error
+    if (acronymExists) {
+      throw new Error(`Acronym \`${acronymExists.acronym}\` already exists and has id \`${acronymExists.id}\``)
+    }
+  }
+
+  // update acronym
+  const updatedAcronym = await prisma.acronym.update({
+    where: {
+      id,
+    },
+    data: {
+      acronym,
+      definition,
+    }
+  })
+
+  // return updated acronym
+  res.status(200).json({
+    success: true,
+    error: null,
+    results: updatedAcronym,
+  })
 })
