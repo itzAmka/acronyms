55c7f1b6e813d728cce5d2c50693ab12566a353b
Amka <Aminhassen1470@gmail.com>
2023-10-14 19:27:15 -0400

    impletement delete acronym functionality

--- controllers/acronyms/deleteAcronym.ts
+++ controllers/acronyms/deleteAcronym.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import asyncHandler from 'express-async-handler';
+import { prisma } from '../../config/prisma'
 
 /**
  * @controller Delete Acronym
@@ -10,5 +11,35 @@ import asyncHandler from 'express-async-handler';
  * @access Public
  */
 export const deleteAcronym = asyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Delete acronym' })
+  // get id from req params
+  const { id } = req.params
+
+  // check if acronym exists
+  const acronymExists = await prisma.acronym.findUnique({
+    where: {
+      id,
+    }
+  })
+
+  // if acronym does not exist, throw error
+  if (!acronymExists) {
+    throw new Error(`Acronym with id \`${id}\` does not exist`)
+  }
+
+  // delete acronym
+  const deletedAcronym = await prisma.acronym.delete({
+    where: {
+      id,
+    }
+  }).catch((_) => { 
+    // handle errors
+    throw new Error(`Error while deleting acronym with id \`${id}\``)
+  })
+
+  // return deleted acronym
+  res.status(200).json({
+    success: true,
+    error: null,
+    results: deletedAcronym,
+  })
 })
